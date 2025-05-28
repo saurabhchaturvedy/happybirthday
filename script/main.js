69e0bc85b8d32df111c1bd6612b7b21ec0b75e22
Chaturvedi, Saurabh <saurabhchaturvedy@gmail.com>
2025-05-29 04:07:16 +0530

    change musi fiel

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Do you want to play music in the background?',
+        title: 'Hey! Lets have some magic on your special day :)',
         icon: 'warning',
         showCancelButton: false,
         confirmButtonColor: '#3085d6',
